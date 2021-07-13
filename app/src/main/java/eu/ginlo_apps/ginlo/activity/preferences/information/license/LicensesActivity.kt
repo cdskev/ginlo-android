47b8c8abf7018354bfe8a6092729102c2c2c222c
Karsten Schramm <kschramm@ginlo.net>
2021-07-13 11:38:02 +0200

    Update license information

--- app/src/main/java/eu/ginlo_apps/ginlo/activity/preferences/information/license/LicensesActivity.kt
+++ app/src/main/java/eu/ginlo_apps/ginlo/activity/preferences/information/license/LicensesActivity.kt
@@ -13,7 +13,7 @@ class LicensesActivity : PreferencesBaseActivity() {
         recyclerview_third_party_licenses.layoutManager = LinearLayoutManager(this)
         recyclerview_third_party_licenses.adapter = LicensesAdapter(this, getLicenses())
         recyclerview_third_party_licenses.setHasFixedSize(true)
-        recyclerview_third_party_licenses.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+        recyclerview_third_party_licenses.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.HORIZONTAL))
     }
 
     override fun getActivityLayout(): Int {
@@ -24,23 +24,24 @@ class LicensesActivity : PreferencesBaseActivity() {
 
     private fun getLicenses(): List<LicenseItem> {
         return mutableListOf(
-            LicenseItem(getString(R.string.greendao), getString(R.string.license_greendao)),
-            LicenseItem(getString(R.string.ezvcard), getString(R.string.license_ezvcard)),
-            LicenseItem(getString(R.string.gson), getString(R.string.license_apache_generic)),
-            LicenseItem(getString(R.string.jbrycpt), getString(R.string.license_jbrycpt)),
-            LicenseItem(getString(R.string.emojicon), getString(R.string.license_emojicon)),
-            LicenseItem(getString(R.string.shortcut_badge), getString(R.string.license_shortcut_badge)),
-            LicenseItem(getString(R.string.cropimage), getString(R.string.license_cropimage)),
-            LicenseItem(getString(R.string.jitsimeet), getString(R.string.apache_license_2_0)),
-            LicenseItem(getString(R.string.photoview), getString(R.string.apache_license_2_0)),
-            LicenseItem(getString(R.string.sqlcipher), getString(R.string.license_sqlcipher)),
-            LicenseItem(getString(R.string.material_icons), getString(R.string.apache_license_2_0)),
-            LicenseItem(getString(R.string.slf4j), getString(R.string.slf4j_license)),
-            LicenseItem(getString(R.string.android_logback), getString(R.string.apache_license_2_0)),
-            LicenseItem(getString(R.string.kotlinx_coroutines), getString(R.string.kotlinx_coroutines_license)),
-            LicenseItem(getString(R.string.dagger), getString(R.string.apache_license_2_0)),
-            LicenseItem(getString(R.string.fab_speed_dial), getString(R.string.apache_license_2_0)),
-            LicenseItem(getString(R.string.zxing_android_embedded), getString(R.string.zxing_android_embedded_license))
+                LicenseItem(getString(R.string.android_logback), getString(R.string.apache_license_2_0)),
+                LicenseItem(getString(R.string.cropimage), getString(R.string.license_cropimage)),
+                LicenseItem(getString(R.string.dagger), getString(R.string.apache_license_2_0)),
+                LicenseItem(getString(R.string.emojicon), getString(R.string.license_emojicon)),
+                LicenseItem(getString(R.string.exoplayer), getString(R.string.apache_license_2_0)),
+                LicenseItem(getString(R.string.ezvcard), getString(R.string.license_ezvcard)),
+                LicenseItem(getString(R.string.fab_speed_dial), getString(R.string.apache_license_2_0)),
+                LicenseItem(getString(R.string.greendao), getString(R.string.license_greendao)),
+                LicenseItem(getString(R.string.gson), getString(R.string.license_gson)),
+                LicenseItem(getString(R.string.jbrycpt), getString(R.string.license_jbrycpt)),
+                LicenseItem(getString(R.string.jitsimeet), getString(R.string.license_jitsimeet)),
+                LicenseItem(getString(R.string.kotlinx_coroutines), getString(R.string.license_kotlinx_coroutines)),
+                LicenseItem(getString(R.string.material_icons), getString(R.string.apache_license_2_0)),
+                LicenseItem(getString(R.string.photoview), getString(R.string.apache_license_2_0)),
+                LicenseItem(getString(R.string.shortcut_badge), getString(R.string.license_shortcut_badge)),
+                LicenseItem(getString(R.string.slf4j), getString(R.string.license_slf4j)),
+                LicenseItem(getString(R.string.sqlcipher), getString(R.string.license_sqlcipher)),
+                LicenseItem(getString(R.string.zxing_android_embedded), getString(R.string.license_zxing))
         )
     }
 }
